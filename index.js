ce4306250aa0f0bded6e5349cf3f5d63df0d1d83
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-06-10 21:13:22 -0400

    just got started

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+const server= require('./server')
+const express =require ('express')
+
+
+const port=4000
+
+server.listen(port, () => {
+    console.log( `\n*** Server Running on http://localhost:${port} ***\n` ); 
+  });
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
+const userRouter=require('./posts/postRouter')
+const postRouter = require('./users/userRouter')
+
 const server = express();
+const port=4000
+
+
+server.use(express.json())
+
+server.use((req,res, next)=>{
+  const time= new Date().toISOString()
+  console.log(`${time}  ${req.method} ${req.url}`)
+  next()
+})
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
